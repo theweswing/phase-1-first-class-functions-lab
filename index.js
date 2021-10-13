d14396e4b74114529e18e4c1cc69ae1a5524cea9
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-10-13 12:00:58 -0400

    commit tree 5396788471b0af9bfc2ed78f7a6563dcdca25f31

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (function(array){
+return [array[0],array[1]]
+})
+
+const returnLastTwoDrivers = (function(array){
+    return array.slice(-2)
+    })
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return (function(fare){
+        return multiplier*fare})
+    }
+
+function fareDoubler(fare){
+    return(createFareMultiplier(2))(fare)}
+
+function fareTripler(fare){
+    return(createFareMultiplier(3))(fare)}
+
+function selectDifferentDrivers(array,returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(array)
+}
